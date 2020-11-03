79fdf0400c3828041bf66493d2d3deac91bd9e91
Brenox889 <brenomendes889@hotmail.com>
2020-11-03 18:37:26 -0300

    feat: Appointment Repository

---
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,29 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    // percorre todos os appointments e verifica se o appointment passado tem
+    // conflito de horario com algum outro
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+    return findAppointment || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
