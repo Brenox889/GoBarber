2bbac01c9974a549483d6ab835a26c6087be87f5
Brenox889 <brenomendes889@hotmail.com>
2020-11-24 23:40:02 -0300

    refactor: remove try catch

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -19,22 +19,18 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { providerId, date } = request.body;
+  const { providerId, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createdAppointment = new CreateAppointmentService();
+  const createdAppointment = new CreateAppointmentService();
 
-    const appointment = await createdAppointment.execute({
-      date: parsedDate,
-      providerId,
-    });
+  const appointment = await createdAppointment.execute({
+    date: parsedDate,
+    providerId,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
